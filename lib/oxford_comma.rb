97f4c26a2237b25f731961f210e055c067aa6258
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-22 18:44:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   str = ""
   array.each_with_index {|word, idx|
-    if idx - 1 == array.length
+    if idx == array.length
       str << "and #{word}"
     else
       str << "#{word}, "
